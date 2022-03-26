5bb3810fec15c41919a90687d99a2f9db91c04f9
erikgna <79864179+erikgna@users.noreply.github.com>
2022-03-25 22:02:47 -0300

    modificações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { QuizzesListComponent } from './quizzes_list/quizzes_list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/modules/material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    QuizzesListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quizzes_list/quizzes_list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-quizzes-list',
+    templateUrl: './quizzes_list.component.html',
+    styleUrls: ['./quizzes_list.component.css']
+})
+export class QuizzesListComponent{
+}
